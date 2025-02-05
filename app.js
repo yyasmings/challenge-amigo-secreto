32cdac39d4b0df87a87a2e0c2146cb2b6590b69e
Yasmin Gomes <yasmingomes956@gmail.com>
2025-02-05 19:19:05 -0300

    Implementa uma função para agregar amigos

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-let nomes = [];
+let amigos = [];
+
+function adicionarAmigos() {
+   let inserirNomes = document.getElementById("amigo");
+   let salvarNomes = inserirNomes.value.trim();
+
+   if (salvarNomes !== "") {
+      amigos.push(salvarNomes);
+      atualizarLista();
+      inserirNomes.value = "";
+   } else {
+      alert("Por favor, insira um nome!")
+   }
+}
+
+function atualizarLista() {
+   let lista = document.getElementById("listaAmigos");
+   lista.innerHTML = "";
+}
