aea919c65f4b6005a71f5961c2543e009fe9d138
Yasmin Gomes <yasmingomes956@gmail.com>
2025-02-05 19:28:40 -0300

    Implemeta uma função para atualizar a lista de amigos

--- app.js
+++ app.js
@@ -16,4 +16,10 @@ function adicionarAmigos() {
 function atualizarLista() {
    let lista = document.getElementById("listaAmigos");
    lista.innerHTML = "";
+
+   for (let i = 0; i < amigos.length; i++) {
+      let item = document.createElement("li");
+      item.textContent = amigos[i];
+      lista.appendChild(item);
+   }
 }
